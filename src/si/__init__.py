d17bd049173e2bf0714c6bcf2af6fe0313b2e52b
Gonçalo Gomes <goncalogomes999@gmail.com>
2026-09-21 10:38:07 +0100

    Update author and maintainer information

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "Diogo Cachetas"
-__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
+__author__ = "Gonçalo Gomes"
+__credits__ = ["Gonçalo Gomes"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Diogo Cachetas"
-__email__ = "diogo.cachetas@inl.int"
+__maintainer__ = "Gonçalo Gomes"
+__email__ = "goncalogomes999@gmail.com"
 
 
 
