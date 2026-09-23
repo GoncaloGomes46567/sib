f8eb4330cf79530f36a173ff0b68b1ae4a8e3149
Gonçalo Gomes <goncalogomes999@gmail.com>
2026-09-23 17:41:44 +0100

    Remove docstring from rmse function
    Removed docstring from rmse function.

--- src/si/metrics/rmse.py
+++ src/si/metrics/rmse.py
@@ -2,23 +2,7 @@ import numpy as np
 
 
 def rmse(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-    """
-    Computes the Root Mean Squared Error (RMSE) between real and predicted values.
-
-        RMSE = sqrt( sum((y_true_i - y_pred_i)^2) / N )
-
-    Parameters
-    ----------
-    y_true: numpy.ndarray
-        The real values of y
-    y_pred: numpy.ndarray
-        The predicted values of y
-
-    Returns
-    -------
-    rmse: float
-        The error between y_true and y_pred.
-    """
+ 
     y_true = np.asarray(y_true)
     y_pred = np.asarray(y_pred)
     return np.sqrt(np.sum((y_true - y_pred) ** 2) / len(y_true))
