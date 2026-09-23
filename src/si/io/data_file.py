0c855c4a0d17a417f2607ac3d6e3feae13712b80
Gonçalo Gomes <goncalogomes999@gmail.com>
2026-09-23 17:41:12 +0100

    Update data_file.py

--- src/si/io/data_file.py
+++ src/si/io/data_file.py
@@ -4,22 +4,6 @@ from si.data.dataset import Dataset
 
 
 def read_data_file(filename: str, sep: str = ",", label: bool = False) -> Dataset:
-    """
-    Reads a data file (no header) and returns a Dataset object.
-
-    Parameters
-    ----------
-    filename: str
-        Name/path of the file
-    sep: str
-        The value separator
-    label: bool
-        Whether the file has y (assumed to be the last column)
-
-    Returns
-    -------
-    Dataset
-    """
     data = np.genfromtxt(filename, delimiter=sep)
 
     if label:
@@ -33,20 +17,7 @@ def read_data_file(filename: str, sep: str = ",", label: bool = False) -> Datase
 
 
 def write_data_file(filename: str, dataset: Dataset, sep: str = ",", label: bool = False) -> None:
-    """
-    Writes a Dataset object to a data file (no header).
-
-    Parameters
-    ----------
-    filename: str
-        Name/path of the file
-    dataset: Dataset
-        Dataset object to write to the file
-    sep: str
-        The value separator
-    label: bool
-        Whether to write y
-    """
+ 
     if label and dataset.has_label():
         y = dataset.y.reshape(-1, 1)
         data = np.hstack((dataset.X, y))
